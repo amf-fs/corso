6f5f5f5cca5d6e8f98f5a56f875459e9c3ac2db3
Alan Morais Fialho <alan.fialho@cargosprint.com>
2025-08-08 15:12:50 -0300

    new account formdesign

--- web/src/app/app.component.ts
+++ web/src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MAT_FORM_FIELD_DEFAULT_OPTIONS, MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet, 
+    MatButtonModule, 
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule],
+  providers: [
+    {provide: MAT_FORM_FIELD_DEFAULT_OPTIONS, useValue: { appearance: 'outline' }}
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })

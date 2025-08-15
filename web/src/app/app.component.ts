b64175935a2eb6798ce6bc3686e6a388d506d86b
Alan Morais Fialho <alan.fialho@cargosprint.com>
2025-08-15 16:55:11 -0300

    Added the account list view with signals

--- web/src/app/app.component.ts
+++ web/src/app/app.component.ts
@@ -1,10 +1,12 @@
-import { Component, signal } from '@angular/core';
+import { Component, signal, WritableSignal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MAT_FORM_FIELD_DEFAULT_OPTIONS, MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Account } from './app.model';
+import { MatListModule } from '@angular/material/list';
 
 @Component({
   selector: 'app-root',
@@ -15,7 +17,8 @@ import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
     MatFormFieldModule,
     MatInputModule,
     MatIconModule, 
-    ReactiveFormsModule],
+    ReactiveFormsModule,
+    MatListModule],
   providers: [
     {provide: MAT_FORM_FIELD_DEFAULT_OPTIONS, useValue: { appearance: 'outline' }}
   ],
@@ -27,4 +30,19 @@ export class AppComponent {
   usernameControl = new FormControl('', [Validators.required]);
   passwordControl = new FormControl('', [Validators.required]);
 
+  accounts: WritableSignal<Account[]> = signal<Account[]>([]);
+
+  onSaveButtonClick(){
+    const account: Account = {
+      name: this.accountNameControl.value as string,
+      username: this.usernameControl.value as string,
+      password: this.passwordControl.value as string
+    } 
+
+    this.accounts.update(accounts => [...accounts, account]);
+
+    this.accountNameControl.reset();
+    this.usernameControl.reset();
+    this.passwordControl.reset();
+  }
 }
